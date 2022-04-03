330ff49583d8dd84992a49aa9894cce35a8ef8e2
e-complaint100 <sahilparmar0571@gmail.com>
2022-04-03 09:59:32 +0530

    Solved infinite lopp after email change

--- app/Http/Middleware/UserAuth.php
+++ app/Http/Middleware/UserAuth.php
@@ -21,8 +21,7 @@ class UserAuth
             if (Auth::user()->role_id == 1) {
                 return redirect('/admin');
             } else {
-                if(Auth::user()->status == 1){
-
+                if((Auth::user()->status == 1) || (Auth::user()->status == 0)){
                     return $next($request);
                 }else{
                     return redirect('/login')->with('failed','You are not allowed to login please contact admin.');
